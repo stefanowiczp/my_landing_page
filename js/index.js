3d8442fb476a786feb18bb143c5baa4bd4005770
Piotr Stefanowicz <stefan8357@gmail.com>
2017-09-26 03:20:56 +0200

    changing view after clicking menu item

---
+++ js/index.js
@@ -0,0 +1,26 @@
+var homeButton = document.getElementById('home');
+var skillsButton = document.getElementById('skills');
+var contactButton = document.getElementById('contact');
+var homeSection = document.getElementsByClassName('home--container')[0];
+var skillsSection = document.getElementsByClassName('skills--container')[0];
+var contactSection = document.getElementsByClassName('contact--container')[0];
+
+
+var show = function (element, section) {
+  homeSection.style.display = "none";
+  skillsSection.style.display = "none";
+  contactSection.style.display = "none";
+  section.style.display = "flex";
+  document.getElementsByClassName('active')[0].classList.remove('active');
+  element.classList.add('active');
+};
+
+homeButton.addEventListener("click", function () {
+  show(homeButton, homeSection)
+});
+skillsButton.addEventListener("click", function () {
+  show(skillsButton, skillsSection)
+});
+contactButton.addEventListener("click", function () {
+  show(contactButton, contactSection)
+});
