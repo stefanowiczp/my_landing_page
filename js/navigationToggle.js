a7c8011d4f116a07aef8ff09c1303e024662cc52
Piotr Stefanowicz <stefan8357@gmail.com>
2017-10-07 03:57:39 +0200

    animation added to mobile menu collapse

--- js/navigationToggle.js
+++ js/navigationToggle.js
@@ -1,10 +1,19 @@
 "use strict";
 (function () {
   var menu = document.querySelector('.menu');
+  var menuItemLinks = document.querySelectorAll('.menu--item a');
+
   var menuCollapsed = document.querySelector('.menu__collapsed');
   menuCollapsed.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     menu.classList.contains('menu-mobile') ? menu.classList.remove('menu-mobile') : menu.classList.add('menu-mobile')
   }
+
+  menuItemLinks.forEach(function (element) {
+    element.addEventListener('click', function () {
+      menu.classList.add('menu-mobile');
+    })
+  })
+
 })();
